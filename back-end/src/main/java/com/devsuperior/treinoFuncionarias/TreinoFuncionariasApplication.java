0cb34f202795395dc42946a573413e8d8990bfb2
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-10-03 19:59:27 -0300

    Inicio do projeto Funcionarias

---
+++ back-end/src/main/java/com/devsuperior/treinoFuncionarias/TreinoFuncionariasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.treinoFuncionarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoFuncionariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoFuncionariasApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/devsuperior/treinoFuncionarias/TreinoFuncionariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.treinoFuncionarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoFuncionariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
