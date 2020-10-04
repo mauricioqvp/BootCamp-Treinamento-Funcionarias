677fba9e2cc22575e74c130fef694e1fedad895c
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-10-04 20:56:36 -0300

    Update, ok

--- back-end/src/main/java/com/devsuperior/treinoFuncionarias/services/FuncionariasService.java
+++ back-end/src/main/java/com/devsuperior/treinoFuncionarias/services/FuncionariasService.java
@@ -3,6 +3,8 @@ package com.devsuperior.treinoFuncionarias.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,4 +39,18 @@ public class FuncionariasService {
 		entity = repository.save(entity);
 		return new FuncionariasDTO(entity);
 	}
+
+	@Transactional
+	public FuncionariasDTO update(Long id, FuncionariasDTO dto) {
+		try {
+			Funcionarias entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setPhoneNumber(dto.getPhoneNumber());
+			entity = repository.save(entity);
+		return new FuncionariasDTO(entity);
+		} catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id não encontrado " + id);
+		}
+	}
+
 }
